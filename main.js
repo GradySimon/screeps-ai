87578f33e514c524f0aabd0c5a421b321cf6e180
GradySimon <grady@gradysimon.com>
2014-11-26 13:03:56 -0800

    Slightly closer to working.
    No more compile or runtime errors, but the creeps and spawns don't seem to be doing anything yet.

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 var _ = require('lodash');
 
+/*
+ * Terminology:
+ * - "behavior": a function that takes only an actor (a creep, spawn, or something else 
+ *               that can "do stuff") and when applied to that actor each tick, will
+ *               direct that actor to do something continuously. Behavior function names
+ *               end with "Behavior".
+ * 
+ * - "behavior generator": a function that takes some parameters and returns a behavior. 
+ *                         Behavior generator functions end with "BehaviorGen".
+
+
 /* In a given room, returns the length of the shortest path between the from and 
  * to positions. opts is an optional param object that will be passed to Room.findPath,
  * which finds the shortest path. */
@@ -19,7 +30,7 @@ function nearestTarget(creep, targetType) {
 
 /* A function that will order the specified creep to continuously
  * harvest energy from the nearest source and return it to the nearest spawn. */
-function workerHarvestTask(creep) {
+function workerHarvestBehavior(creep) {
     if (creep.energy < creep.energyCapacity) {
         var nearestSource = nearestTarget(creep, Game.SOURCE);
         creep.harvest(nearestSource);
@@ -28,9 +39,13 @@ function workerHarvestTask(creep) {
         creep.moveTo(spawn);
         creep.transferEnergy(spawn);
     }
-} 
+}
 
-var bodyPartCosts = new Map(
+var creepSpecs = {
+    Harvester: [Game.MOVE, Game.WORK, Game.MOVE, Game.WORK, Game.CARRY]
+};
+
+var bodyPartCosts = new Map([
         [Game.MOVE, 50],
         [Game.WORK, 20],
         [Game.CARRY, 50],
@@ -38,7 +53,7 @@ var bodyPartCosts = new Map(
         [Game.RANGED_ATTACK, 150],
         [Game.HEAL, 200],
         [Game.TOUGH, 5]
-    );
+    ]);
 
 /* Computes the energy cost of the creep specification specified by creepSpec */    
 function computeCreepCost(creepSpec) {
@@ -47,16 +62,20 @@ function computeCreepCost(creepSpec) {
     });
 }
 
-/* Orders the given spawn to create a creep according to the given creepSpec */
-function spawnCreateCreepTask(spawn, creepSpec) {
-    var cost = computeCreepCost(creepSpec);
-    if (spawn.energy >= cost) {
-       spawn.createCreep(creepSpec);
-    }
+/* Returns a task that orders the given spawn to create a creep according to the given creepSpec */
+function spawnCreateCreepBehaviorGen(creepSpec) {
+    return function(spawn) {
+        var cost = computeCreepCost(creepSpec);
+        if (spawn.energy >= cost) {
+            spawn.createCreep(creepSpec);
+        }
+    };
 } 
 
-var creeps = Game.creeps
-var spawns = Game.spawns
+var creeps = Game.creeps;
+var spawns = Game.spawns;
 
-_.forEach(creeps, workerHarvestTask);
-_.forEach(spawns, spawnCreateCreepTask);
+var spawnCreateHarvesterBehavior = spawnCreateCreepBehaviorGen(creepSpecs.Harvester);
+
+_.forEach(creeps, workerHarvestBehavior);
+_.forEach(spawns, spawnCreateHarvesterBehavior);
