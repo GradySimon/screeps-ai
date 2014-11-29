559aa9fda099740345a6090f1dea6d93113ef9ea
Grady Simon <grady@gradysimon.com>
2014-11-29 13:16:13 -0800

    Trivial resource manager (accepts all plans).

---
+++ resources.js
@@ -0,0 +1,6 @@
+var _ = require('lodash');
+
+module.exports.evaluate = function(plans) {
+    // TODO: trivial evaluator so I can focus on testing Objectives.
+    return { 'accepted': plans };
+};
