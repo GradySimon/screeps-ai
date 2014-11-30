5677a180351ce5e16138f0e501697ee27f3e1bc4
Grady Simon <grady@gradysimon.com>
2014-11-29 22:40:13 -0800

    Substantial refactoring to generatePlan. Added comments.

--- objectives.js
+++ objectives.js
@@ -1,3 +1,5 @@
+/* global Game, require, module */
+
 var _ = require('lodash');
 var utils = require('utils');
 var behaviors = require('behaviors');
@@ -18,9 +20,17 @@ function Plan(resourceSpec, policy) {
     this.policy = policy;
 }
 
-// ---- The Growth Objective ----
+//////////////////////////
+// The Growth Objective //
+//////////////////////////
 
-module.exports.GrowthObjective = function(room) {
+/**
+ * Represents the objective of growth. Generates plans that will emphasize
+ * harvesting energy and creating more harvester creeps.
+ * @constructor
+ * @param {Room} The room that this GrowthObjective should operate in.
+ */
+var GrowthObjective = module.exports.GrowthObjective = function(room) {
     this.room = room;
     this.harvesterSelector = {
         rooms: [room],
@@ -28,45 +38,22 @@ module.exports.GrowthObjective = function(room) {
     };
 };
 
-module.exports.GrowthObjective.prototype.generatePlan = function() {
+/**
+ * Generates a plan that will assign creeps to harvest from all available
+ * sources and will create new creeps if necessary to harvest best.
+ * @return {Plan}
+ */
+GrowthObjective.prototype.generatePlan = function() {
     var harvesters = utils.selectCreeps(this.harvesterSelector);
     var sources = this.room.find(Game.SOURCES);
-    var sourceToCreepAssignments = new Map(_.map(sources, function(source) { 
-        return [source, []]; 
-    }));
-    _.forEach(harvesters, function(harvester) {
-        var sortedSources = utils.sortByDistanceFrom(harvester, sources);
-        _.forEach(sortedSources, function(source) {
-            var currentAssignments = sourceToCreepAssignments.get(source);
-            if (currentAssignments.length < MAX_HARVESTERS_PER_SOURCE) {
-                currentAssignments.push(harvester);
-                sourceToCreepAssignments.set(source, currentAssignments);
-                return false;
-            }
-        });
-    });
-    var creepToSourceAssignments = new Map();
-    for (var assignment of sourceToCreepAssignments.entries()) {
-        var source = assignment[0];
-        var assignedHarvesters = assignment[1];
-        _.forEach(assignedHarvesters, function (harvester) {
-            creepToSourceAssignments.set(harvester, source);
-        });
-    }
-    var numCreepsRequested = creepToSourceAssignments.size;
-    var creepsRequested = utils.keyArray(creepToSourceAssignments);
-    var sourcesRequested = utils.keyArray(sourceToCreepAssignments);
-    var harvesterDeficit = sources.length * MAX_HARVESTERS_PER_SOURCE
-                           - numCreepsRequested;
-    var spawnsRequested = [];
-    if (harvesterDeficit > 0) {
-        // TODO: request more spawns if harvesterDeficit > 1;
-        spawnsRequested = [this.room.find(Game.MY_SPAWNS)[0]];
-    }
+    var assignments = this.getAssignments(harvesters, sources);
+    var creepsRequested = assignments.creepsUsed;
+    var sourcesRequested = assignments.sourcesUsed;
+    var spawnsRequested = getSpawnsRequested(sources, harvesters);
     var resourceSpec = new ResourceSpec(creepsRequested, sourcesRequested, spawnsRequested);
     var policy = function() {
         _.forEach(creepsRequested, function(creep) {
-            var assignedSource = creepToSourceAssignments.get(creep);
+            var assignedSource = assignments.sourceToCreeps.get(creep);
             behaviors.workerHarvestBehaviorGen(assignedSource)(creep);
         });
         _.forEach(spawnsRequested, function(spawn) {
@@ -75,3 +62,62 @@ module.exports.GrowthObjective.prototype.generatePlan = function() {
     };
     return new Plan(resourceSpec, policy);
 };
+
+/**
+ * Returns a Map of the form (source -> list of creeps) representing a proposal
+ * for how to assign harvesters to sources.
+ * @param  {Collection of creeps} harvesters Available harvesters
+ * @param  {Collection of sources} sources Available sources
+ * @return {Object} An object of the form {sourcesUsed: Array, creepsUsed:
+ * Array, sourcesToCreeps: Map, creepToSource: Map}
+ */
+GrowthObjective.prototype.getAssignments = function(harvesters, sources) {
+    var sourceToCreepsMap = new Map(_.map(sources, function(source) { 
+        return [source, []]; 
+    }));
+    var creepToSourceMap = new Map();
+    _.forEach(harvesters, function(harvester) {
+        var sortedSources = utils.sortByDistanceFrom(harvester, sources);
+        _.forEach(sortedSources, function(source) {
+            var roster = sourceToCreepsMap.get(source);
+            if (roster.length < MAX_HARVESTERS_PER_SOURCE) {
+                roster.push(harvester);
+                sourceToCreepsMap.set(source, roster);
+                creepToSourceMap.set(harvester, source);
+                return false; // break the _.forEach loop
+            }
+        });
+    });
+    return {
+        sourcesUsed: utils.keyArray(sourceToCreepsMap),
+        creepsUsed: utils.keyArray(creepToSourceMap),
+        sourceToCreeps: sourceToCreepsMap,
+        creepToSource: creepToSourceMap
+    };
+};
+
+/**
+ * Chooses spawns to create new harvesters from, if necessary
+ * @param  {Collection of sources} accessibleSources
+ * @param  {Collection of creeps} accessibleHarvesters
+ * @return {Array of spawns} An array of spawns that should create harvesters
+ */
+function getSpawnsRequested(accessibleSources, accessibleHarvesters) {
+    var spawnsRequested = [];
+    if (harvesterDeficit(accessibleSources.length, accessibleHarvesters.length) > 0) {
+        // TODO: request more spawns if harvesterDeficit > 1;
+        spawnsRequested = [this.room.find(Game.MY_SPAWNS)[0]];
+    }
+    return spawnsRequested;
+}
+
+/**
+ * Calculates how many additional harvesters the room needs to harvest
+ * most effectively.
+ * @param  {number} numSources
+ * @param  {number} numHarvesters
+ * @return {number}
+ */
+function harvesterDeficit(numSources, numHarvesters) {
+    return numSources * MAX_HARVESTERS_PER_SOURCE - numHarvesters;
+}
